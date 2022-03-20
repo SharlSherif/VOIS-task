d2adec21be79d9f01fa721c718cf7ae6ea7367ab
Sharl Sherif <dev.sharl.sherif@gmail.com>
2022-03-20 11:36:18 +0200

    config file for setting up the Youtube api endpoint & key

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  API: "https://youtube.googleapis.com/youtube/v3/search",
+  key: "AIzaSyBzcvXXtHlos1XnBxFsZqO6Nmqd0jR5kOQ",
+};
