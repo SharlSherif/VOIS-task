a8cca1d694488f95fb395a0886e7dc01670bb70d
Sharl Sherif <dev.sharl.sherif@gmail.com>
2022-03-20 11:35:22 +0200

    packages files & redux store setup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-
+import videoReducer from "../redux/slicers/videosSlice"
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    videos: videoReducer
   },
 });
 
